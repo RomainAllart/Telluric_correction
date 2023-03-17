5e4f2e43fcb927f86cadc45d794c8bfa99545118
RomainAllart <44673364+RomainAllart@users.noreply.github.com>
2023-03-16 20:28:52 -0400

    Update run_telluric_correction.py

--- run_telluric_correction.py
+++ run_telluric_correction.py
@@ -18,10 +18,11 @@ import multiprocessing
 files_nirps_drs=glob.glob('Telluric_corrected_files/r.NIRPS.*S2D_BLAZE_A.fits')
 files_nirps_drs.sort()
 
+instrument = 'NIRPS_drs'
 molecules = ['H2O','O2','CO2','CH4'] # molecules to consider
 save_path='Telluric_corrected_files/' # directory to save files
 save_options = ['DRS', 'Extended', 'Telluric'] # Files to be saved
 
 if __name__ == "__main__":
-    ATC.multiprocessing_ATC(files_nirps_drs,['NIRPS_drs', molecules, save_path, save_options],nthreads=max(1,multiprocessing.cpu_count()-2))
+    ATC.multiprocessing_ATC(files_nirps_drs,[instrument, molecules, save_path, save_options],nthreads=max(1,multiprocessing.cpu_count()-2))
 

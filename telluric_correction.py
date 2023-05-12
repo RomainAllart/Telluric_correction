88ad49274ffb23fdf7bc07ea164477dcb8d61f75
xdumusque <x.dumusque@gmail.com>
2023-05-12 15:11:59 +0200

    debugging

--- telluric_correction.py
+++ telluric_correction.py
@@ -151,6 +151,7 @@ def supergauss(x, ew, expo):
     #Expression corrige avec la logique de depary
     str_exp = 'exp(-0.5 * abs(x / ew) ** expo)'
 
+    logger.info('USING A TOTAL OF %s THREADS TO PERFORM TELLURIC CORRECTION', ne.nthreads)
 
     return ne.evaluate(str_exp)
 
@@ -384,7 +385,7 @@ def fit_telluric_model(Parameters,rv,data=['wave','flux','database','qt_list','l
             
             ew = (fwhm / 2) / (2 * np.log(2)) ** (1 / expo)
 
-            logger.info("range_scan = (-%i,%i,1)", range_scan, range_scan)
+            logger.info('USING A TOTAL OF %s THREADS TO PERFORM TELLURIC CORRECTION', ne.nthreads)
             for offset in range(-range_scan,range_scan):
                 dv = (data_wave_temp/np.roll(data_wave_temp,offset)-1)*c_km_s
                 w = supergauss(dv, ew, expo)
